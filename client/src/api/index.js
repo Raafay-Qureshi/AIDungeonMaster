9a514e0811c8779695692db81daae7c31444d481
Raafay-Qureshi <superraafay@gmail.com>
2025-08-07 19:58:49 -0400

    fixed url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import useAuthStore from '../store/authStore';
+const apiUrl = import.meta.env.VITE_API_URL;
 
-const API_URL = 'http://localhost:1002/api';
+const API_URL = `${apiUrl}/api`;
 
 const api = axios.create({
   baseURL: API_URL,
 });
 
-// Add a request interceptor to include the token in all requests
 api.interceptors.request.use((config) => {
   const token = useAuthStore.getState().token;
   if (token) {
